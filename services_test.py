65bb10cd7d38126c4463eb323566cc89e6ed69b0
Kaushik2k13 <34117911+Kaushik2k13@users.noreply.github.com>
2019-12-03 23:00:04 +0100

    Update and rename serveces_test.py to services_test.py

--- services_test.py
+++ services_test.py
@@ -1,14 +1,14 @@
 """
-This module contains a class TestSum which tests for all the success and failure 
+This module contains a class TestSum which tests for all the success and failure
 cases of file management system
 """
+import os
 import unittest
 from server_logic import Adminservices
 from server_logic import Userservices
-import os
 
 class TestSum(unittest.TestCase):
-    """This module contains the class TestSum 
+    """This module contains the class TestSum
         Methods:
         ---------------
             test_folder_creation(self):
@@ -16,7 +16,7 @@ class TestSum(unittest.TestCase):
             test_write_file(self):
                 tests for writing and clearing in a file
             test_change_folder_admin(self):
-                checks whether the directory have been changed and if the users directory 
+                checks whether the directory have been changed and if the users directory
                 is out of the predefined directory then the access will be denied
             test_change_folder_user(self):
                 checks whether the directory have been changed and if the users directory is
@@ -32,15 +32,11 @@ class TestSum(unittest.TestCase):
         """
         tests for the success and failure cases of folder creation
         """
-        client = Userservices(
-                    os.getcwd(),
-                    os.getcwd(),
-                    'pawan',
-                    '12345'
-                )
+        client = Userservices(os.getcwd(), os.getcwd(), 'pawan',
+                              '12345')
         inputvalues = [
-            ['asdf','admin'],
-            ['asdf','admin']
+            ['asdf', 'admin'],
+            ['asdf', 'admin']
         ]
         expectedvalues = [
             'folder created',
@@ -48,23 +44,19 @@ class TestSum(unittest.TestCase):
         ]
         result = []
         for inputv in inputvalues:
-            result.append(client.create_folder(inputv[0],inputv[1]))
+            result.append(client.create_folder(inputv[0], inputv[1]))
         self.assertListEqual(result, expectedvalues)
-    
+
     def test_write_file(self):
         """
         tests for writing,editing in a file and clearing the file
         """
-        client = Userservices(
-                    os.getcwd(),
-                    os.getcwd(),
-                    'pawan',
-                    '12345'
-                )
+        client = Userservices(os.getcwd(), os.getcwd(), 'pawan',
+                              '12345')
         inputvalues = [
-            ['testfile.txt','hello world'],
-            ['testfile.txt','second statement'],
-            ['testfile.txt',None]
+            ['testfile.txt', 'hello world'],
+            ['testfile.txt', 'second statement'],
+            ['testfile.txt', None]
         ]
         expectedvalues = [
             'file edited successfully',
@@ -73,30 +65,26 @@ class TestSum(unittest.TestCase):
         ]
         result = []
         for inputv in inputvalues:
-            result.append(client.write_file(inputv[0],inputv[1]))
+            result.append(client.write_file(inputv[0], inputv[1]))
         self.assertListEqual(result, expectedvalues)
 
     def test_change_folder_admin(self):
         """
-        checks whether the directory have been changed and if the users directory 
+        checks whether the directory have been changed and if the users directory
         is out of the predefined directory then the access will be denied
         """
-        userpath = os.path.join(os.getcwd(),'pawan')
-        client = Userservices(
-                    os.getcwd(),
-                    userpath,
-                    'pawan',
-                    '12345'
-                )
+        userpath = os.path.join(os.getcwd(), 'pawan')
+        client = Userservices(os.getcwd(), userpath, 'pawan',
+                              '12345')
         inputvalues = [
-            ['group','admin'],
-            ['testfolder','admin'],
-            ['..','admin'],
-            ['..','admin'],
-            ['..','admin'],
+            ['group', 'admin'],
+            ['testfolder', 'admin'],
+            ['..', 'admin'],
+            ['..', 'admin'],
+            ['..', 'admin'],
         ]
         pathlist = [
-            os.path.join(userpath,'group'),
+            os.path.join(userpath, 'group'),
             userpath,
             os.path.dirname(userpath)
         ]
@@ -110,29 +98,25 @@ class TestSum(unittest.TestCase):
         ]
         result = []
         for inputv in inputvalues:
-            result.append(client.change_directory(inputv[0],inputv[1]))
+            result.append(client.change_directory(inputv[0], inputv[1]))
         self.assertListEqual(result, expectedvalues)
-    
+
     def test_change_folder_user(self):
         """
         checks whether the directory have been changed and if the users directory is
         out of the predefined directory then the access will be denied.
         """
-        userpath = os.path.join(os.getcwd(),'pawan')
-        client = Userservices(
-                    os.getcwd(),
-                    userpath,
-                    'pawan',
-                    '12345'
-                )
+        userpath = os.path.join(os.getcwd(), 'pawan')
+        client = Userservices(os.getcwd(), userpath, 'pawan',
+                              '12345')
         inputvalues = [
-            ['group','user'],
-            ['testfolder','user'],
-            ['..','user'],
-            ['..','user'],
+            ['group', 'user'],
+            ['testfolder', 'user'],
+            ['..', 'user'],
+            ['..', 'user'],
         ]
         pathlist = [
-            os.path.join(userpath,'group'),
+            os.path.join(userpath, 'group'),
             userpath
         ]
         expectedvalues = [
@@ -143,21 +127,17 @@ class TestSum(unittest.TestCase):
         ]
         result = []
         for inputv in inputvalues:
-            result.append(client.change_directory(inputv[0],inputv[1]))
+            result.append(client.change_directory(inputv[0], inputv[1]))
         self.assertListEqual(result, expectedvalues)
-    
+
     def test_read_file(self):
         """
         checks whether the file reads the text from a folder and whether the file
-        exists 
+        exists
         """
-        userpath = os.path.join(os.getcwd(),'pravallika')
-        client = Userservices(
-                    os.getcwd(),
-                    userpath,
-                    'pawan',
-                    '12345'
-                )
+        userpath = os.path.join(os.getcwd(), 'pravallika')
+        client = Userservices(os.getcwd(), userpath, 'pawan',
+                              '12345')
         inputvalues = [
             [None],
             ['test.txt'],
@@ -182,16 +162,12 @@ class TestSum(unittest.TestCase):
         tests for the user deletion and the user cannot delete their self
         """
 
-        client = Adminservices(
-                    os.getcwd(),
-                    os.getcwd(),
-                    'pawan',
-                    '12345'
-                )
+        client = Adminservices(os.getcwd(), os.getcwd(), 'pawan',
+                               '12345')
         inputvalues = [
-            ['pawan','12345','admin'],
-            ['lasya','23423','user'],
-            ['lasya','12345','user']
+            ['pawan', '12345', 'admin'],
+            ['lasya', '23423', 'user'],
+            ['lasya', '12345', 'user']
         ]
         expectedvalues = [
             'You cannot delete your self',
@@ -200,7 +176,7 @@ class TestSum(unittest.TestCase):
         ]
         result = []
         for inputv in inputvalues:
-            result.append(client.delete_user(inputv[0],inputv[1],inputv[2]))
+            result.append(client.delete_user(inputv[0], inputv[1], inputv[2]))
         self.assertListEqual(result, expectedvalues)
 if __name__ == '__main__':
     unittest.main()
